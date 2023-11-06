323e9b451c70209b1a8cd9af0c5bf9923906e3cb
SAADUMAR121 <112772287+SAADUMAR121@users.noreply.github.com>
2023-11-06 07:33:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id',
+SESSION_ID: process.env.SESSION_ID || 'ZEROTWO=5u9yEagb#8ja4fxVUpCgxVNkTbeU3SExn_jmprDVo-AnM8vIULyE',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'youre POSTGRESQL url',
 LANG: process.env.BOT_LANG || 'EN' ,
 ANTI_BAD: process.env.ANTI_BAD || 'false',
@@ -15,6 +15,6 @@ ONLY_GROUP: process.env.ONLY_GROUP || 'false',
 ANTI_LINK: process.env.ANTI_LINK || 'false' ,
 ANTI_BOT: process.env.ANTI_BOT || 'false',
 ALIVE: process.env.ALIVE || `default`,
-FOOTER: process.env.FOOTER ||  '©𝚉𝙴𝚁𝙾-𝚃𝚆𝙾 𝙼𝙳',
-LOGO: process.env.LOGO || `https://github.com/vihangayt0/ZeroTwo-Uploads/blob/main/ZERO-TWO.png?raw=true` 
+FOOTER: process.env.FOOTER ||  'Mr Bot',
+LOGO: process.env.LOGO || `https://graph.org/file/56c0fb63b1eb1b2ca789c.jpg` 
 };
